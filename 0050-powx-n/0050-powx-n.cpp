2eca9ddbac86a1e1b3d135096807fa25f0b56bcd
Priyansh Jain <141728911+Priyansh1867@users.noreply.github.com>
2026-08-12 10:10:49 +0530

    Time: 0 ms (100%), Space: 8.4 MB (93.57%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
+    double findpow(double x , int n)
+    {
+        //Base Case ,agar power 0 h toh power 1 return kr dege
+        if(n == 0) return 1;
+        //a me x ki power n/2 store kr lege
+        double a = findpow(x,n/2);
+        // agar power even h toh return a*a 
+        if(n%2 == 0) return a*a;
+        // agar power odd h toh return a*a*x;
+        else return a*a*x;
+    }
     double myPow(double x, int n) {
-        double ans = pow(x,n);
-        return ans;
+        //agar power 0 h toh power 1 return kr dege
+        if(n == 0) return 1;
+        //agar n>0 h toh power func ko call krege
+        else if(n > 0) return findpow(x,n);
+        else // if(n < 0)
+        {
+            //kyuki n<0 h toh power negative me h
+            //TLE se bachne ke liye long long use krege aur usme power store kr lege 
+            long long b = n;
+            // b ko -1 se multiply kr dege taki wo positive ho
+            b *= -1;
+            //jo bhi power hogi wo 1/power hoegi
+            return 1/findpow(x,b);
+        }
     }
 };
