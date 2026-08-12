273b2923bfe8f464292ade7daa3be41c4603de77
Priyansh Jain <141728911+Priyansh1867@users.noreply.github.com>
2026-08-12 09:35:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (50.51%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,47 @@
+//method 1
+class Solution
+{
+    public:
+    bool isPowerOfTwo(int n)
+    {
+        if(n <= 0) return false;
+        while(n % 2 == 0)
+        {
+            n /= 2;
+        }
+        return n==1;
+    }
+};
+
+/*
+method 2
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        for (int i=0 ; i<31 ; i++){
+            int ans = pow(2,i);
+            if (ans == n){
+                return true;
+            }
+        }return false;
+    }
+};
+
+//method 3
+
+class Solution {
+    public : 
+    bool isPowerOfTwo(int n){
+        int ans = 1;
+        for(int i =0;i<31;i++){
+            if (ans == n ){
+                return true;
+            }
+            if(ans < INT_MAX/2){
+            ans = ans * 2;
+            }
+        }return false;
+    }
+};
+*/
