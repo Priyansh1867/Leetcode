db0e8d32cb2800f3d5b87df714bc4d9ee06634d9
Priyansh Jain <141728911+Priyansh1867@users.noreply.github.com>
2026-08-14 13:50:07 +0530

    Time: 0 ms (100%), Space: 12.4 MB (35.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,25 +1,52 @@
+//Three Pointer approach
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans(m + n);
-        int i = 0, j = 0, k = 0;
+        //i , j ko last pr rakho 
+        int i = m-1;
+        int j = n-1;
+        int k = m + n -1;
 
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                ans[k++] = nums1[i++];
-
-            } else {
-                ans[k++] = nums2[j++];
+        // Compare elements from the back and place the larger one at index k
+        while(i >= 0 && j >= 0)
+        {
+            if(nums1[i] > nums2[j])
+            {
+                nums1[k--] = nums1[i--];
+            }
+            else
+            {
+                nums1[k--] = nums2[j--];
             }
         }
-        while (i < m) {
-            ans[k++] = nums1[i++];
-        }
-        while (j < n) {
-            ans[k++] = nums2[j++];
-        }
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = ans[i];
+        // Copy any remaining elements from nums2 into nums1
+        // (Remaining elements in nums1 are already sorted in their correct places)
+        while(j >= 0)
+        {
+            nums1[k--] = nums2[j--];
         }
     }
 };
+/*
+Approach :- Brute Force , in this sorted arrays ko wapas sort kr rha hu isse time complexity optimised nhi h .
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>merged(n+m);
+        for(int i=0 ; i<m ; i++)
+        {
+            merged[i] = nums1[i];
+        }
+        for(int i=0 ; i<n ; i++)
+        {
+            merged[m+i] = nums2[i];
+        }
+        sort(merged.begin() ,merged.end());
+
+        for(int i=0 ; i<m+n ; i++)
+        {
+            nums1[i] = merged[i];
+        }
+    }
+};
+*/
